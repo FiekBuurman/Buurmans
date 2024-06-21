ffbb9f3655de2f4ab2983a4a7a6a89bb76ecc440
Fiek Buurman <fiek@marti-orbak.nl>
2024-06-21 16:27:33 +0200

    using observermanager

--- Buurmans.Console/Application.cs
+++ Buurmans.Console/Application.cs
@@ -1,10 +1,36 @@
-﻿namespace Buurmans.Console;
+﻿using System;
+using Buurmans.Common.Extensions;
+using Buurmans.Common.Interfaces;
 
-public class Application : IApplication
+namespace Buurmans.Console;
+
+public class Application(IObserverManager observerManager) : IApplication
 {
 	public void Run()
 	{
-		System.Console.WriteLine("Press any key to exit.");
+		RegisterObservers();
+
+		observerManager.NotifyChange(new Exception("Big Bad Test Exception!"));
+		observerManager.NotifyChange("Press any key to exit.");
+		
 		System.Console.ReadKey();
+
+        UnRegisterObservers();
 	}
+	
+	private void RegisterObservers()
+	{
+		observerManager.Register<string>(WriteToConsole);
+		observerManager.Register<Exception>(WriteToConsole);
+	}
+
+	private void UnRegisterObservers()
+	{
+		observerManager.Unregister<string>(WriteToConsole);
+		observerManager.Unregister<Exception>(WriteToConsole);
+	}
+
+    private static void WriteToConsole(Exception exception) => System.Console.WriteLine(exception.FlattenException());
+
+	private static void WriteToConsole(string message) => System.Console.WriteLine(message.AddTimePrefix());
 }
