89b5cbcb201056feb0ac634da1e612a1b0651a5b
Fiek Buurman <fiek@marti-orbak.nl>
2024-06-21 16:27:16 +0200

    added

---
+++ Buurmans.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+namespace Buurmans.Common.Extensions
+{
+    public static class StringExtensions
+	{
+		public static string AddTimePrefix(this string info) => $"{TimePrefix()}{info}";
+
+		private static string TimePrefix() => DateTime.UtcNow.ToString("[HH:mm:ss.ff] - ", CultureInfo.InvariantCulture);
+	}
+}
