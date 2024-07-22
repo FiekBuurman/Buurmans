c4a3db5589dfbb8b8f833a241906cfc9828215b2
Fiek <fiekbuurman@gmail.com>
2024-07-22 18:17:06 +0200

    soon to remove

--- Buurmans.Mqtt/MqttPublisher.cs
+++ Buurmans.Mqtt/MqttPublisher.cs
@@ -1,45 +1,45 @@
-﻿using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using Buurmans.Mqtt.Models;
-using Buurmans.Mqtt.Providers;
-using MQTTnet;
-using MQTTnet.Client;
+﻿//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using Buurmans.Mqtt.Models;
+//using Buurmans.Mqtt.Providers;
+//using MQTTnet;
+//using MQTTnet.Client;
 
-namespace Buurmans.Mqtt
-{
-    internal class MqttPublisher
-    {
-		private readonly MqttConfigurationSettingsModel _config;
+//namespace Buurmans.Mqtt
+//{
+//    internal class MqttPublisher
+//    {
+//		private readonly MqttConfigurationSettingsModel _config;
 
-		public MqttPublisher()
-		{
-			var configSettingsProvider = new MqttConfigurationProvider();
-			_config = configSettingsProvider.GetSettings();
-		}
+//		public MqttPublisher()
+//		{
+//			var configSettingsProvider = new MqttConfigurationProvider();
+//			_config = configSettingsProvider.GetSettings();
+//		}
 
-		public async Task PublishMessageAsync(string message)
-		{
-			var factory = new MqttFactory();
-			var mqttClient = factory.CreateMqttClient();
+//		public async Task PublishMessageAsync(string message)
+//		{
+//			var factory = new MqttFactory();
+//			var mqttClient = factory.CreateMqttClient();
 
-			var options = new MqttClientOptionsBuilder()
-				.WithClientId(_config.ClientId)
-				.WithTcpServer(_config.BrokerUrl, _config.Port)
-				.WithCredentials(_config.Username, _config.Password)
-				.Build();
+//			var options = new MqttClientOptionsBuilder()
+//				.WithClientId(_config.ClientId)
+//				.WithTcpServer(_config.BrokerUrl, _config.Port)
+//				.WithCredentials(_config.Username, _config.Password)
+//				.Build();
 
-			await mqttClient.ConnectAsync(options, CancellationToken.None);
+//			await mqttClient.ConnectAsync(options, CancellationToken.None);
 
-			var mqttMessage = new MqttApplicationMessageBuilder()
-				.WithTopic(_config.Topic)
-				.WithPayload(Encoding.UTF8.GetBytes(message))
-				.WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
-				.Build();
+//			var mqttMessage = new MqttApplicationMessageBuilder()
+//				.WithTopic(_config.Topic)
+//				.WithPayload(Encoding.UTF8.GetBytes(message))
+//				.WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+//				.Build();
 
-			await mqttClient.PublishAsync(mqttMessage, CancellationToken.None);
+//			await mqttClient.PublishAsync(mqttMessage, CancellationToken.None);
 
-			await mqttClient.DisconnectAsync();
-		}
-    }
-}
+//			await mqttClient.DisconnectAsync();
+//		}
+//    }
+//}
