009f35b9a506f0845fba11fed63b0ab43304ab06
Fiek Buurman <fiek@marti-orbak.nl>
2024-07-26 10:46:57 +0200

    moved scrape.UI from HTML.Scraper.Ui

---
+++ Buurmans.Scrape.Ui/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Buurmans.Scrape.Core;
+
+namespace Buurmans.Scrape.Ui;
+
+public class Application(IHtmlScrapeEngine htmlScrapeEngine) : IApplication
+{
+	public void Run()
+	{
+		var result = htmlScrapeEngine.Process().Result;
+		Console.WriteLine(result);
+		Console.WriteLine("Press any key to exit.");
+		Console.ReadKey();
+	}
+}
