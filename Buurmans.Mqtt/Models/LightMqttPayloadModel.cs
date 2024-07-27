fdc700bc80d0340b9df2110d4e5ec901a7bb71a2
Fiek Buurman <fiek@marti-orbak.nl>
2024-07-27 20:00:52 +0200

    Moved Models

---
+++ Buurmans.Mqtt/Models/LightMqttPayloadModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Buurmans.Mqtt.Models
+{
+	public class LightMqttPayloadModel : MqttPayloadModel
+	{
+		[JsonProperty("state")]
+		public string State { get; set; }
+
+		[JsonProperty("color")]
+		public MqttLightColorModel ColorModel { get; set; }
+	}
+}
---
+++ Buurmans.Mqtt/Models/MqttLightColorModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Buurmans.Mqtt.Models
+{
+	public class MqttLightColorModel
+	{
+		[JsonProperty("r")]
+		public int Red { get; set; }
+
+		[JsonProperty("g")]
+		public int Green { get; set; }
+
+		[JsonProperty("b")]
+		public int Blue { get; set; }
+	}
+}
