b20b98e3448a94d25aa6f7c8e969add94330c610
Fiek Buurman <fiek@marti-orbak.nl>
2024-07-28 19:19:55 +0200

    added missing registrations

--- Buurmans.AmbiLight.Core/AutofacModule.cs
+++ Buurmans.AmbiLight.Core/AutofacModule.cs
@@ -9,6 +9,8 @@ public class AutofacModule : Module
 {
 	protected override void Load(ContainerBuilder builder)
 	{
+		builder.RegisterModule<Buurmans.Common.AutofacModule>();
+		builder.RegisterModule<Buurmans.Mqtt.AutofacModule>();
 		builder.RegisterType<ScreenCaptureService>().As<IScreenCaptureService>().SingleInstance();
 		builder.RegisterType<ColorCalculationService>().As<IColorCalculationService>().SingleInstance();
 		builder.RegisterType<SettingsModelProvider>().As<ISettingsModelProvider>().SingleInstance();
