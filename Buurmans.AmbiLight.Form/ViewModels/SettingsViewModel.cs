6fe3b32ab0bacac22df6345e4f3c2025959ce567
Fiek <fiekbuurman@gmail.com>
2024-07-28 20:54:19 +0200

    added observer messaging

--- Buurmans.AmbiLight.Form/ViewModels/SettingsViewModel.cs
+++ Buurmans.AmbiLight.Form/ViewModels/SettingsViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using Buurmans.AmbiLight.Core.Interfaces;
 using Buurmans.AmbiLight.Core.Models;
 using Buurmans.AmbiLight.Form.Interfaces;
+using Buurmans.Common.Extensions;
+using Buurmans.Common.Interfaces;
 using Buurmans.Mqtt;
 using Buurmans.Mqtt.Models;
+using System;
+using System.Text;
 
 namespace Buurmans.AmbiLight.Form.ViewModels;
 
-internal class SettingsViewModel(ISettingsModelProvider settingsModelProvider, IMqttEngine mqttEngine) : ISettingsViewModel
+internal class SettingsViewModel(ISettingsModelProvider settingsModelProvider, IMqttEngine mqttEngine, IObserverManager observerManager) : ISettingsViewModel
 {
 	private ISettingsView _settingsView;
 
@@ -35,7 +39,28 @@ internal class SettingsViewModel(ISettingsModelProvider settingsModelProvider, I
 
 	public void TestMqttSettingsButtonPressed(MqttConfigurationSettingsModel mqttConfigurationSettingsModel)
 	{
-		mqttEngine.InitSettings(mqttConfigurationSettingsModel);
+		observerManager.Register<string>(AddObserverMessage);
+        observerManager.Register<Exception>(AddObserverMessage);
+
+        mqttEngine.InitSettings(mqttConfigurationSettingsModel);
 		mqttEngine.TestSettings(mqttConfigurationSettingsModel);
-	}
+
+        observerManager.Unregister<string>(AddObserverMessage);
+        observerManager.Unregister<Exception>(AddObserverMessage);
+
+		_settingsView.ShowMessage(_messageBuilder.ToString());
+		_messageBuilder.Clear();
+    }
+
+	private void AddObserverMessage(Exception exception)
+	{
+		AddObserverMessage(exception.FlattenException());
+    }
+
+    private void AddObserverMessage(string message)
+	{
+        _messageBuilder.Append(message);
+    }
+
+	private StringBuilder _messageBuilder = new StringBuilder();
 }
