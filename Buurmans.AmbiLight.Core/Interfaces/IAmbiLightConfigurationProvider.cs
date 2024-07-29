11af4ca3a34186062a3b947f7e9ce8b5d106f10b
Fiek Buurman <fiek@marti-orbak.nl>
2024-07-29 10:49:24 +0200

    implemented IBaseConfigurationProvider

---
+++ Buurmans.AmbiLight.Core/Interfaces/IAmbiLightConfigurationProvider.cs
@@ -0,0 +1,9 @@
+﻿using Buurmans.AmbiLight.Core.Models;
+using Buurmans.Common.Interfaces;
+
+namespace Buurmans.AmbiLight.Core.Interfaces;
+
+internal interface IAmbiLightConfigurationProvider : IBaseConfigurationProvider<AmbilLightConfigurationSettingsModel>
+{
+	new AmbilLightConfigurationSettingsModel GetSettings();
+}
---
+++ Buurmans.AmbiLight.Core/Providers/AmbiLightConfigurationProvider.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Buurmans.AmbiLight.Core.Interfaces;
+using Buurmans.AmbiLight.Core.Models;
+using Buurmans.Common.Converters;
+using Buurmans.Common.Providers;
+using Buurmans.Mqtt.Models;
+
+namespace Buurmans.AmbiLight.Core.Providers;
+
+internal class AmbiLightConfigurationProvider(IJsonConverter jsonConverter) 
+	: BaseConfigurationProvider<AmbilLightConfigurationSettingsModel>(jsonConverter), IAmbiLightConfigurationProvider
+{
+	public new AmbilLightConfigurationSettingsModel GetSettings()
+	{
+		var settings = base.GetSettings();
+		return settings ?? CreateDefaultSettings();
+	}
+
+	private AmbilLightConfigurationSettingsModel CreateDefaultSettings()
+	{
+		var settingsModel = new AmbilLightConfigurationSettingsModel
+		{
+			PixelSkipSteps = 10,
+			DelayInMilliseconds = 1000,
+			ColorSettingModels = CreateColorSettingModels(),
+			MqttConfigurationSettingsModel = CreateDefaultMqttSettings()
+		};
+		
+		SaveSettings(settingsModel);
+		return settingsModel;
+	}
+
+	private static MqttConfigurationSettingsModel CreateDefaultMqttSettings()
+	{
+		return new MqttConfigurationSettingsModel
+		{
+			BrokerUrl = "127.0.0.1",
+			Port = 1883,
+			Topic = "MyTopic",
+			ClientId = "MyClientId",
+			UserName = "MyUserName",
+			Password = "MyPassword",
+			Timeout = 60
+		};
+	}
+
+	private static List<AmbiLightColorSettingModel> CreateColorSettingModels()
+	{
+		return CreateDefaultColorList().Select(color => new AmbiLightColorSettingModel
+			{
+				Color = color,
+				Allowed = true
+			})
+			.ToList();
+	}
+
+	private static IEnumerable<Color> CreateDefaultColorList()
+	{
+		var colorList = new List<Color>
+		{
+			Color.FromArgb(255, 64, 0),
+			Color.FromArgb(255, 128, 0),
+			Color.FromArgb(255, 192, 0),
+			Color.FromArgb(255, 255, 0),
+			Color.FromArgb(192, 255, 0),
+			Color.FromArgb(128, 255, 0),
+			Color.FromArgb(64, 255, 0),
+			Color.FromArgb(0, 255, 0),
+			Color.FromArgb(0, 255, 64),
+			Color.FromArgb(0, 255, 128),
+			Color.FromArgb(0, 255, 192),
+			Color.FromArgb(0, 255, 255),
+			Color.FromArgb(0, 192, 255),
+			Color.FromArgb(0, 128, 255),
+			Color.FromArgb(0, 64, 255),
+			Color.FromArgb(0, 0, 255),
+			Color.FromArgb(64, 0, 255),
+			Color.FromArgb(128, 0, 255),
+			Color.FromArgb(192, 0, 255),
+			Color.FromArgb(255, 0, 255),
+			Color.FromArgb(255, 0, 192),
+			Color.FromArgb(255, 0, 128),
+			Color.FromArgb(255, 0, 64),
+			Color.FromArgb(255, 0, 0)
+		};
+
+		return colorList;
+	}
+}
