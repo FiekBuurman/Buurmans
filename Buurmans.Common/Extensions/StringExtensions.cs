8bb77dced27ebba82bbce90133d0b68d55a50ee6
Fiek Buurman <fiek@marti-orbak.nl>
2024-07-31 12:02:39 +0200

    fixed wrong time format

--- Buurmans.Common/Extensions/StringExtensions.cs
+++ Buurmans.Common/Extensions/StringExtensions.cs
@@ -7,6 +7,6 @@ namespace Buurmans.Common.Extensions
 	{
 		public static string AddTimePrefix(this string info) => $"{TimePrefix()}{info}";
 
-		private static string TimePrefix() => DateTime.UtcNow.ToString("[HH:mm:ss.ff] - ", CultureInfo.InvariantCulture);
+		private static string TimePrefix() => DateTime.Now.ToString("[HH:mm:ss.ff] - ", CultureInfo.InvariantCulture);
 	}
 }
