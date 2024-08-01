ad8d30964c1c65e5ef58e31d253860d5f28e8817
Fiek Buurman <fiek@marti-orbak.nl>
2024-08-01 17:03:31 +0200

    refactors

--- Buurmans.Common/Loggers/Logger.cs
+++ Buurmans.Common/Loggers/Logger.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
 using Buurmans.Common.Enums;
 using Buurmans.Common.Extensions;
@@ -12,89 +11,86 @@ namespace Buurmans.Common.Loggers;
 
 public class Logger : ILogger
 {
-    private string _logFileFullPath = string.Empty;
-    private LogLevelType _logLevelType = LogLevelType.All;
+	private string _logFileFullPath = string.Empty;
+	private LogLevelType _logLevelType = LogLevelType.All;
 
 	public void Info(string message) => LogMessage(LogLevelType.Info, message);
 
-    public void Warning(string message ) => LogMessage(LogLevelType.Warning, message);
-    
-    public void Error(Exception exception) => LogMessage(LogLevelType.Error, exception.FlattenException());
+	public void Warning(string message) => LogMessage(LogLevelType.Warning, message);
 
-    public void SetLogLevels(LogLevelType logLevelType)
+	public void Error(Exception exception) => LogMessage(LogLevelType.Error, exception.FlattenException());
+
+	public void SetLogLevels(LogLevelType logLevelType)
 	{
 		_logLevelType = logLevelType;
-        LogMessage(LogLevelType.All, $"LogLevelType set to: {logLevelType}");
+		LogMessage(LogLevelType.All, $"LogLevelType set to: {logLevelType}");
 	}
 
-	private void LogMessage(LogLevelType logLevelType, string formattedMessage )
+	private void LogMessage(LogLevelType logLevelType, string message)
 	{
-		formattedMessage = $"{logLevelType}: {formattedMessage.AddTimePrefix()}";
-        
-        if (IsDebug()) 
+		var formattedMessage = CreateLogMessage(logLevelType, message);
+
+		if (IsDebugMode())
 			Trace.WriteLine(formattedMessage);
-		
-        if (!ShouldLog(logLevelType))
-            return;
 
-        EnsureLogFile();
+		if (!ShouldLog(logLevelType))
+			return;
 
-		Task.Run(() => TryAppendText(formattedMessage));
+		EnsureLogFile();
+		_ = AppendLogAsync(formattedMessage);
+	}
+
+	private string CreateLogMessage(LogLevelType logLevelType, string message)
+	{
+		return $"{logLevelType}: {message.AddTimePrefix()}";
+	}
+
+	private async Task AppendLogAsync(string message)
+	{
+		const int maxRetries = 10;
+		for (var retry = 0; retry < maxRetries; retry++)
+		{
+			try
+			{
+				using var streamWriter = new StreamWriter(_logFileFullPath, append: true);
+				await streamWriter.WriteLineAsync(message);
+				break;
+			}
+			catch (IOException)
+			{
+				await Task.Delay(100);
+			}
+			catch (Exception ex)
+			{
+				Trace.WriteLine($"Error writing to log file: {ex.Message}");
+				Trace.WriteLine(ex.StackTrace);
+				break;
+			}
+		}
 	}
 
 	private bool ShouldLog(LogLevelType messageLogLevel)
 	{
-		if (messageLogLevel == LogLevelType.All)
-			return true;
-
-		return _logLevelType switch
+		return messageLogLevel == LogLevelType.All || _logLevelType switch
 		{
 			LogLevelType.NoLog => false,
 			LogLevelType.All => true,
 			LogLevelType.Error => messageLogLevel == LogLevelType.Error,
-            LogLevelType.Info => messageLogLevel is LogLevelType.Info or LogLevelType.Warning or LogLevelType.Error,
+			LogLevelType.Info => messageLogLevel is LogLevelType.Info or LogLevelType.Warning or LogLevelType.Error,
 			LogLevelType.Warning => messageLogLevel is LogLevelType.Warning or LogLevelType.Error,
-			
 			_ => throw new ArgumentOutOfRangeException(nameof(_logLevelType), _logLevelType, "Invalid log level type.")
 		};
 	}
-    
-    private void TryAppendText(string message)
-    {
-        var retryCount = 0;
-        const int maxRetries = 10;
 
-        while (retryCount < maxRetries)
-        {
-            try
-			{
-				using var streamWriter = new StreamWriter(_logFileFullPath, append: true);
-				streamWriter.WriteLine(message);
-				break;
-			}
-            catch (IOException)
-            {
-                retryCount++;
-                Thread.Sleep(500); // Wait for the file to be released by the previous thread
-            }
-            catch (Exception ex)
-            {
-                Trace.WriteLine($"Error writing to log file: {ex.Message}");
-                Trace.WriteLine(ex.StackTrace);
-                break;
-            }
-        }
-    }
+	private void EnsureLogFile()
+	{
+		if (string.IsNullOrEmpty(_logFileFullPath) || !File.Exists(_logFileFullPath))
+		{
+			_logFileFullPath = TextFileLogHelper.GetOrCreateLogFileFullPath();
+		}
+	}
 
-    private void EnsureLogFile()
-    {
-        if (string.IsNullOrEmpty(_logFileFullPath) || !File.Exists(_logFileFullPath))
-        {
-            _logFileFullPath = TextFileLogHelper.GetOrCreateLogFileFullPath();
-        }
-    }
-
-	private static bool IsDebug()
+	private static bool IsDebugMode()
 	{
 #if DEBUG
 		return true;
