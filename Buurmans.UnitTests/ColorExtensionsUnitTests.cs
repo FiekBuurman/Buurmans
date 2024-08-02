271a6869b37b3e63e23a2117267f44eecc8ab853
Fiek Buurman <fiek@marti-orbak.nl>
2024-08-02 16:01:54 +0200

    added more tests

---
+++ Buurmans.UnitTests/ColorExtensionsUnitTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using Buurmans.Common.Extensions;
+
+namespace Buurmans.UnitTests
+{
+	[TestFixture]
+	public class ColorExtensionsUnitTests
+	{
+		[Test]
+		public void ToRgbString_WhenColorIsFromArgb_ShouldReturnCorrectString()
+		{
+			// Arrange
+			var color = Color.FromArgb(255, 128, 64);
+
+			// Act
+			var result = color.ToRgbString();
+			const string expected = "Color [R=255, G=128, B=64]";
+
+            // Assert
+			Assert.That(result, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void ToRgbString_WhenColorIsEmpty_ShouldHandleDefaultColor()
+		{
+			// Arrange
+			var color = Color.Empty;
+
+			// Act
+			var result = color.ToRgbString();
+			const string expected = "Color [R=0, G=0, B=0]";
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void ToRgbString_WhenColorIsNamed_ShouldReturnCorrectStringForNamedColor()
+		{
+			// Arrange
+			var color = Color.Red;
+
+			// Act
+			var result = color.ToRgbString();
+			const string expected = "Color [R=255, G=0, B=0]";
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+		}
+	}
+}
--- Buurmans.UnitTests/ExceptionExtensionUnitTests.cs
+++ Buurmans.UnitTests/ExceptionExtensionUnitTests.cs
@@ -1,40 +1,129 @@
 using Buurmans.Common.Extensions;
+using System.Net;
 
-namespace Buurmans.UnitTests
+namespace Buurmans.UnitTests;
+
+public class ExceptionExtensionUnitTests
 {
-    public class ExceptionExtensionUnitTests
-    {
-		private const string ExceptionMessage = "Test message";
-		private const string ExceptionSource = "Test source";
+	private const string ExceptionMessage = "Test message";
+	private const string ExceptionSource = "Test source";
 
-        [Test]
-		public void FlattenException_WhenExceptionIsNull_ShouldReturnEmptyString()
+	[Test]
+	public void FlattenException_WhenExceptionIsNull_ShouldReturnEmptyString()
+	{
+		// Arrange
+		var expected = string.Empty;
+
+		// Act
+		var actual = ExceptionExtension.FlattenException(null);
+
+		// Assert
+		Assert.That(actual, Is.EqualTo(expected));
+	}
+
+	[Test]
+	public void FlattenException_WithSingleException_ShouldReturnFormattedString()
+	{
+		// Arrange
+		var exception = new Exception(ExceptionMessage) { Source = ExceptionSource };
+
+		// Act
+		var result = exception.FlattenException();
+
+		// Assert
+		Assert.Multiple(() =>
 		{
-            // Arrange
-			var expected = string.Empty;
+			Assert.That(result, Does.Contain(ExceptionMessage), $"Expected to contain: \"{ExceptionMessage}\" in: \r{result}");
+			Assert.That(result, Does.Contain(ExceptionSource), $"Expected to contain: \"{ExceptionSource}\" in: \r{result}");
+		});
+	}
+	
+	[Test]
+	public void FlattenException_ShouldIncludeMessageAndSource_WhenExceptionIsProvided()
+	{
+		// Arrange
+		var exception = new Exception("Test exception message");
 
-			// Act
-			var actual = ExceptionExtension.FlattenException(null);
+		// Act
+		var result = exception.FlattenException();
 
-            // Assert
-            Assert.That(actual, Is.EqualTo(expected));
+		// Assert
+		Assert.That(result, Does.Contain("Message    :\tTest exception message"));
+	}
+
+	[Test]
+	public void FlattenException_ShouldIncludeInnerException()
+	{
+		// Arrange
+		var innerException = new Exception("Inner exception message");
+		var exception = new Exception("Outer exception message", innerException);
+
+		// Act
+		var result = exception.FlattenException();
+
+		// Assert
+		Assert.That(result, Does.Contain("Inner exception message"));
+		Assert.That(result, Does.Contain("Outer exception message"));
+	}
+
+	[Test]
+	public void FlattenException_ShouldHandleExceptionWithoutSourceOrTargetSite()
+	{
+		// Arrange
+		var exception = new Exception("Test exception message");
+
+		// Act
+		var result = exception.FlattenException();
+
+		// Assert
+		Assert.That(result, Does.Contain("No Source!"));
+		Assert.That(result, Does.Contain("No Method!"));
+	}
+
+	[Test]
+	public void FlattenException_ShouldHandleExceptionWithStackTrace()
+	{
+		// Arrange
+		try
+		{
+			throw new Exception("Exception with stack trace");
 		}
-
-		[Test]
-		public void FlattenException_WithSingleException_ShouldReturnFormattedString()
+		catch (Exception ex)
 		{
-			// Arrange
-			var exception = new Exception(ExceptionMessage) { Source = ExceptionSource };
-
 			// Act
-            var result = exception.FlattenException();
+			var result = ex.FlattenException();
 
 			// Assert
-            Assert.Multiple(() =>
-			{
-				Assert.That(result, Does.Contain(ExceptionMessage), $"Expected to contain: \"{ExceptionMessage}\" in: \r{result}");
-				Assert.That(result, Does.Contain(ExceptionSource), $"Expected to contain: \"{ExceptionSource}\" in: \r{result}");
-            });
-        }
-    }
+			Assert.That(result, Does.Contain("Stacktrace :\t"));
+		}
+	}
+
+	[Test]
+	public void FlattenException_ShouldIncludeCurrentDateAndTime()
+	{
+		// Arrange
+		var exception = new Exception("Test exception message");
+
+		// Act
+		var result = exception.FlattenException();
+		var currentDate = DateTime.Now.ToLongDateString();
+		var currentTime = DateTime.Now.ToLongTimeString();
+
+		// Assert
+		Assert.That(result, Does.Contain($"{currentDate}:\tTime    :\t{currentTime}"));
+	}
+
+	[Test]
+	public void FlattenException_ShouldIncludeComputerName()
+	{
+		// Arrange
+		var exception = new Exception("Test exception message");
+
+		// Act
+		var result = exception.FlattenException();
+		var computerName = Dns.GetHostName();
+
+		// Assert
+		Assert.That(result, Does.Contain($"Computer    :\t{computerName}"));
+	}
 }
